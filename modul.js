498f578e69d62fac9f52d8556ce6667a041edcdb
jonkapotkivanok <79459392+jonkapotkivanok@users.noreply.github.com>
2021-03-16 12:16:50 +0100

    modul.js

---
+++ modul.js
@@ -0,0 +1,20 @@
+const összeadás = (n1, n2) => {
+  res = n1 + n2;
+  return res;
+};
+
+const szorzás = (n1, n2) => {
+  res = n1 * n1;
+  return res;
+};
+
+const hatványozás = (n1, n2) => {
+  res = n1 ** n2;
+  return res;
+};
+
+module.exports = {
+  összeadás,
+  szorzás,
+  hatványozás,
+};
